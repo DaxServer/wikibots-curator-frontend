d9bdf37c5c3e7411d99239df50000867a2d3b652
DaxServer <daxserver@icloud.com>
2025-07-03 15:06:51 +0200

    style: remove unnecessary margin in job status

--- src/components/JobStatusTag.vue
+++ src/components/JobStatusTag.vue
@@ -18,10 +18,8 @@ const durationDisplayParts = computed(() => {
       <div>
         <span v-if="status.state !== 'error'">{{ status.text }}</span>
         <template v-if="durationDisplayParts">
-          <span class="ml-1">
-            for {{ durationDisplayParts.hours }}:{{ durationDisplayParts.minutes
-            }}<span class="text-xs">:{{ durationDisplayParts.seconds }}</span>
-          </span>
+          for {{ durationDisplayParts.hours }}:{{ durationDisplayParts.minutes
+          }}<span class="text-xs">:{{ durationDisplayParts.seconds }}</span>
         </template>
       </div>
       <div v-if="status.state === 'error'" class="mt-1 text-xs">
