626a03c7d47df053a1b757f5bc4d918cdb10567f
DaxServer <7479937+DaxServer@users.noreply.github.com>
2025-12-15 12:51:54 +0100

    Revamp Footer layout and add Phabricator link

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -7,32 +7,50 @@ const sourceLinks = [
 </script>
 
 <template>
-  <div class="flex items-center gap-1 max-w-7xl mx-auto mt-10">
-    <i class="pi pi-github"></i>
-    &nbsp;
-    <template
-      v-for="(link, index) in sourceLinks"
-      :key="index"
-    >
+  <div class="flex flex-col gap-4 max-w-7xl mx-auto mt-10">
+    <div class="flex items-center gap-1">
+      <i class="pi pi-github"></i>
+      &nbsp;
+      <template
+        v-for="(link, index) in sourceLinks"
+        :key="index"
+      >
+        <a
+          :href="link.url"
+          target="_blank"
+          rel="noopener noreferrer"
+          class="hover:underline"
+        >
+          {{ link.text }}
+        </a>
+        &nbsp;&bull;&nbsp;
+      </template>
+      <i class="pi pi-share-alt text-sm!"></i>
+      &nbsp;
       <a
-        :href="link.url"
+        href="https://opensource.org/license/mit"
         target="_blank"
         rel="noopener noreferrer"
         class="hover:underline"
       >
-        {{ link.text }}
+        MIT licensed
       </a>
-      &nbsp;&bull;&nbsp;
-    </template>
-    <i class="pi pi-share-alt text-sm!"></i>
-    &nbsp;
-    <a
-      href="https://opensource.org/license/mit"
-      target="_blank"
-      rel="noopener noreferrer"
-      class="hover:underline"
-    >
-      MIT license
-    </a>
+    </div>
+    <div class="flex items-center gap-1">
+      <img
+        src="https://upload.wikimedia.org/wikipedia/commons/a/a4/Favicon-Phabricator-WM.svg"
+        alt="Phabricator logo"
+        class="w-4 h-4"
+      />
+      &nbsp;
+      <a
+        href="https://phabricator.wikimedia.org/tag/tool-curator/"
+        target="_blank"
+        rel="noopener noreferrer"
+        class="hover:underline"
+      >
+        phabricator
+      </a>
+    </div>
   </div>
 </template>
